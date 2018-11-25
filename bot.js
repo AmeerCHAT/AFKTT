0235c4cf2acf1de9ff499de2c8fc205be8d2244a
AmeerCHAT <45131113+AmeerCHAT@users.noreply.github.com>
2018-11-25 14:44:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,51 +1,61 @@
-const settings = require("./config.json"); 
 const Discord = require('discord.js');
-const initcmd = settings.initcmd;
-const id = settings.id;
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Hi ${client.user.tag} , This Code by : Kahrbaa `);
-  console.log('')
-  console.log(`i Have  [ " ${client.guilds.size} " ]`);
-});
-
-const developers = id
-const adminprefix = initcmd;
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + '1')) {
-    client.user.setGame(argresult);
-      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
-  } else 
-  if (message.content.startsWith(adminprefix + '2')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send("**:white_check_mark: | The Watching Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
-  } else 
-  if (message.content.startsWith(adminprefix + '3')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send("**:white_check_mark: | The Listening Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
-  } else 
-  if (message.content.startsWith(adminprefix + '4')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/i_kahrba999");
-      message.channel.send("**:white_check_mark: | The Streaming Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
+  console.log(`Logged in as ${client.user.tag}!`);  
 });
 
 
+client.on('ready', async() => {
+var server = "514356888188485642"; 
+var channel = "516224761647464488";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam**')
+    },305);
+})
 
-client.login(settings.token);
+
+
+
+
+
+client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
+client3.login(process.env.TOKEN3);
+client4.login(process.env.TOKEN4);
+client5.login(process.env.TOKEN5);
+client6.login(process.env.TOKEN6);
+client7.login(process.env.TOKEN7);
+client8.login(process.env.TOKEN8);
+client9.login(process.env.TOKEN9);
+client10.login(process.env.TOKEN10);
+client11.login(process.env.TOKEN11);
+client12.login(process.env.TOKEN12);
+client13.login(process.env.TOKEN13);
+client14.login(process.env.TOKEN14);
+client15.login(process.env.TOKEN15);
+client16.login(process.env.TOKEN16);
+client17.login(process.env.TOKEN17);
+client18.login(process.env.TOKEN18);
+client19.login(process.env.TOKEN19);
+client20.login(process.env.TOKEN20);
+client21.login(process.env.TOKEN21);
+client22.login(process.env.TOKEN22);
+client23.login(process.env.TOKEN23);
+client24.login(process.env.TOKEN24);
+client25.login(process.env.TOKEN25);
+client26.login(process.env.TOKEN26);
+client27.login(process.env.TOKEN27);
+client28.login(process.env.TOKEN28);
+client29.login(process.env.TOKEN29);
+client30.login(process.env.TOKEN30);
+client31.login(process.env.TOKEN31);
+client32.login(process.env.TOKEN32);
+client33.login(process.env.TOKEN33);
+client34.login(process.env.TOKEN34);
+client35.login(process.env.TOKEN35);
+client36.login(process.env.TOKEN36);
+client37.login(process.env.TOKEN37);
+client38.login(process.env.TOKEN38);
+client39.login(process.env.TOKEN39);
+client40.login(process.env.TOKEN40);
